420c8f580ce05e9a8ce345e7213025612d858030
Elizabeth Braatz <elizabethbraatz42@gmail.com>
2025-10-23 15:40:50 -0500

    elizabeth stuff

--- HW8_Jillian_and_Elizabeth.R
+++ HW8_Jillian_and_Elizabeth.R
@@ -9,6 +9,7 @@
 #------------------------------------
 library(rinat) #install a package meant for extracting iNat data 
 library(tidyverse) #adding ggplot2 via tidyverse 
+library(dplyr) #dplyr package for rearranging the dataframe for the pie chart 
 
 #Pulling data from iNat 
 #-------------------------------------
@@ -32,22 +33,74 @@ bees = get_inat_obs(
 # Conducting Exploratory Data Analysis 
 #----------------------------------------------
 
-#First let's see if this is normally distributed 
+#######################
+# GRAPH 1: HISTOGRAM 
+# #####################
 
 hist = ggplot(data = bees) + 
   geom_bar(aes(x = scientific_name)) + 
-  labs(x = "Scientific Name", y = "Count of observations", title = "Histogram of Bees by Species")
+  labs(x = "Scientific Name", y = "Count of observations", title = "Histogram of Bees by Species")+ 
+  theme(axis.text.x = 
+          element_text(angle = 90, vjust = 0.5, hjust = 1)) #rotate the labels 
 hist
 
 # I guess normal distribution doesn't make sense for species name 
 
+#######################
+# GRAPH 2: PIECHART  
+# #####################
+
+bees$scientific_name
+
+species_counts = bees %>% 
+  count(scientific_name)
+species_counts
+
+## pie chart of species proportions
+pie = ggplot(data = species_counts, aes(x = "", 
+                             y = n, 
+                             fill = scientific_name)) +
+  geom_col()+ #creating a barchart with the n column 
+  coord_polar(theta = "y")+ #make it round 
+  labs(title = "piechart of beeeeeees") + #add a title 
+  theme_void() #remove the ugly background 
+
+
+#######################
+# GRAPH 3: MAP   
+# #####################
+
+# Loading additional libraries 
+library(ggmap)
+library(maps)
+library(mapdata)
+library(grid)
+library(ggspatial)
+library(viridis) #lets you create color blind friendly maps
+
+
+#load the state data 
+states <- map_data("state") #load data for each state
+wi <- subset(states, region =="wisconsin") #create object with wi data
+
+#also make df objects with your county lines
+counties <- map_data("county")
+wi_county <- subset(counties, region == "wisconsin")
+
+#Make a blank WI  map 
+
+wi_b <- ggplot(data = wi, mapping = aes(x = long, y = lat, group = group)) + 
+  coord_fixed(1.3) + 
+  geom_polygon(color = "black", fill = "#f0f0f0") #can change background color of map
+
+wi_b #view map
 
 
 
-
-
+##########################################################################33
 
 # Code Graveyard 
 
 # hist(bees$scientific_name) #base R is mad
-
+# geom_text(aes(label = n), 
+        #  position = position_stack(vjust = 0.5))  # I tried adding labels but hte  names were too long 
