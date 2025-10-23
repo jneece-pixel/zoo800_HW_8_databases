c02bfe0a7f6cd3ded25c3ef5a0808c12d7b2ce40
Elizabeth Braatz <elizabethbraatz42@gmail.com>
2025-10-23 15:01:02 -0500

    elizabeth's version of hw we will combine later

--- HW8_Jillian_and_Elizabeth.R
+++ HW8_Jillian_and_Elizabeth.R
@@ -7,7 +7,24 @@
 
 #Loading libraries 
 #------------------------------------
-
+library(rinat) #install a package meant for extracting iNat data 
 
 #Pulling data from iNat 
 #-------------------------------------
+
+
+#We thought about getting data from a project but it had too many things 
+# about_d = get_inat_obs_project("city-nature-challenge-2025-northeast-wisconsin", type = "info", raw = FALSE) #this gets data from iNat project city Nature 2025 NE WI info about the project 
+
+# d = get_inat_obs_project("city-nature-challenge-2025-northeast-wisconsin", type = "observations", raw = FALSE) #This gets the actual data but you can't tell it to not take everything 
+
+#Let's just get bumble bees from WI in 2025, month 5, max 1000 bees 
+bumblebees_in_wi_2025 = get_inat_obs(
+  query = "Bombus", 
+  quality = "research",
+  place_id = "wisconsin", 
+  year = 2025, 
+  month = 5, 
+  maxresults = 500 
+)
+
